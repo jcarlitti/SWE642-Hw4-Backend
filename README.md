5cbfa481fd2f33bc86e79a63df0e8412e8097027
jinal17 <jinal17@gmail.com>
2021-04-21 00:42:42 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# SWE 642 : Software Engineering for the World Wide Web
+
+### Name : Jinal Shah, Justin Carlitti, Lohith Reddy, Manaff Khan
+
+### Homework 3:
+### WebApp Home page: [here]()
+### WebApp Survey page: [here]()
+
+#### Git: URL : [here]()
+
+## Steps for Triggering the Build through Jenkins:
+
+- **First Time User** Clone this Repo using: git clone [here]()
+- Make necessary changes in java files for the Web service on your local system
+- $git status
+- $git add -–all
+- $git commit -m "Pushing Final Changes"
+- $git push
---
+++ src/main/java/com/swe642/service/StudentService.java
@@ -0,0 +1,59 @@
+package com.swe642.service;
+
+import com.swe642.entity.Student;
+import com.swe642.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository repository;
+
+    public Student saveStudent(Student student) {
+        return repository.save(student);
+    }
+
+    public List<Student> saveStudents(List<Student> students) {
+        return repository.saveAll(students);
+    }
+
+    public List<Student> getStudents() {
+        return repository.findAll();
+    }
+
+    public Student getStudentById(int id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public Student getStudentByName(String name) {
+        return repository.findByName(name);
+    }
+
+    public String deleteStudent(int id) {
+        repository.deleteById(id);
+        return "Student removed !! " + id;
+    }
+
+    public Student updateStudent(Student student) {
+//        Student existingStudent = repository.findByName(student.getId()).orElse(null);
+//        existingStudent.setName(student.getName());
+//        existingStudent.setLastname(student.getLastname());
+//        existingStudent.setAddress(student.getAddress());
+//        existingStudent.setCity(student.getCity());
+//        existingStudent.setState(student.getState());
+//        existingStudent.setZip(student.getZip());
+//        existingStudent.setPhone(student.getPhone());
+//        existingStudent.setEmail(student.getEmail());
+//        existingStudent.setDos(student.getDos());
+//        existingStudent.setAbout_college(student.getAbout_college());
+//        existingStudent.setInterested(student.getInterested());
+//        existingStudent.setRecommend(student.getRecommend());
+//        return repository.save(existingStudent);
+        return student;
+    }
+
+
+}
